a32c5a74dd979cdc6f357cd0eea9bc2ed4d00a62
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2023-02-09 05:38:22 +0100

    fff

--- clip/model.py
+++ clip/model.py
@@ -161,7 +161,7 @@ class LayerNorm(nn.LayerNorm):
 
     def forward(self, x: torch.Tensor):
         orig_type = x.dtype
-        ret = super().forward(x.type(torch.float32))
+        ret = self.inner_layernorm(x.type(torch.float32))
         return ret.type(orig_type)
 
 class QuickGELU(nn.Module):
